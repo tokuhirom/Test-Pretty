49b6d2b93fc2d35f95be504b0459bdf9b280cb60
tokuhirom <tokuhirom@gmail.com>
2012-10-23 10:28:45 +0900

    initial import

---
+++ lib/Test/Pretty.pm
@@ -0,0 +1,39 @@
+package Test::Pretty;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Test::Pretty - A module for you
+
+=head1 SYNOPSIS
+
+  use Test::Pretty;
+
+=head1 DESCRIPTION
+
+Test::Pretty is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
