8b541a5842685cc9bbd097869b0a2f110bb82d47
tokuhirom <tokuhirom@gmail.com>
2012-11-07 16:46:52 +0900

    retty.pm

---
+++ lib/App/Prove/Plugin/retty.pm
@@ -0,0 +1,22 @@
+package App::Prove::Plugin::retty;
+use strict;
+use warnings;
+use utf8;
+use parent qw/App::Prove::Plugin::Pretty/;
+
+1;
+__END__
+
+=head1 NAME
+
+App::Prove::Plugin::retty - -Pretty
+
+=head1 SYNOPSIS
+
+    % prove -Pretty
+
+=head1 DESCRIPTION
+
+This is a alias module for App::Prove::Plugin::Pretty.
+
+Using C<< -Pretty >> is bit pretty.
